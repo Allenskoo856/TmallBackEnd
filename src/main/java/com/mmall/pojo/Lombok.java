759d36a925d8a53af40854d7e6896694a9eb11a0
ZongLUnli <mrzonglunli@gmail.com>
2018-04-20 20:22:08 +0800

    changes

---
+++ src/main/java/com/mmall/pojo/Lombok.java
@@ -0,0 +1,17 @@
+package com.mmall.pojo;
+
+/**
+ * @author : Administrator
+ * @create 2018-04-20 16:33
+ */
+public class Lombok {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
--- src/main/java/com/mmall/vo/OrderItemVo.java
+++ src/main/java/com/mmall/vo/OrderItemVo.java
@@ -1,11 +1,17 @@
 package com.mmall.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
 /**
- * Created by geely
+ *
+ * @author Administrator
  */
+@Getter
+@Setter
 public class OrderItemVo {
 
     private Long orderNo;
@@ -22,68 +28,4 @@ public class OrderItemVo {
     private BigDecimal totalPrice;
 
     private String createTime;
-
-    public Long getOrderNo() {
-        return orderNo;
-    }
-
-    public void setOrderNo(Long orderNo) {
-        this.orderNo = orderNo;
-    }
-
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductImage() {
-        return productImage;
-    }
-
-    public void setProductImage(String productImage) {
-        this.productImage = productImage;
-    }
-
-    public BigDecimal getCurrentUnitPrice() {
-        return currentUnitPrice;
-    }
-
-    public void setCurrentUnitPrice(BigDecimal currentUnitPrice) {
-        this.currentUnitPrice = currentUnitPrice;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(BigDecimal totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
 }
